71da95a5a22bb49814a30a32349ef1ed9787505d
XavierCarty95 <xaviercarty@gmail.com>
2020-11-05 00:20:14 -0500

    commit tree 10866acc25e8b64fd71e4d0b4a0288a211b46bcb

--- lib/person.rb
+++ lib/person.rb
@@ -1,95 +1,84 @@
 # your code goes here
 require 'pry'
 class Person
-    attr_reader :name , :happiness ,  :hygiene
-    attr_accessor :bank_account 
-    def initialize(name)
-        @name = name
-        @bank_account = 25
-        @happiness = 8
-        @hygiene = 8
-    end
-    def happiness=(happiness)
-        @happiness = happiness
-        if @happiness > 10
-            @happiness = 10
-        elsif @happiness < 0
-            @happiness = 0
-        end
-    end
-     def hygiene=(hygiene)
-        @hygiene = hygiene
-        if @hygiene > 10
-            @hygiene = 10
-        elsif @hygiene < 0
-            @hygiene = 0
-        end
-    end
+   
+  attr_accessor :account,  :bank_account
+  attr_reader :name, :happiness, :hygiene
 
-    def happy?
-        if @happiness > 7 
-            true
-        else 
-            false 
-        end 
-    end
+  def initialize(name)
+     @name = name
+     @account = account
+     @happiness = 8
+     @hygiene = 8
+     @bank_account = 25
+  end 
 
-    def clean?
-    if    @hygiene > 7 
-       return true 
-    else 
-        return false 
+  def happiness=(happiness)
+     
+   if  happiness > 10 
+       @happiness = 10
+   elsif happiness < 0 
+       @happiness = 0
+   else 
+       @happiness = happiness
     end 
+     
+  end 
+ 
+  def hygiene=(hygiene)
+    if  hygiene > 10 
+        @hygiene = 10
+    elsif hygiene < 0 
+        @hygiene = 0
+    else 
+        @hygiene = hygiene
+     end 
+ end 
+
+ def happy?
+   self.happiness > 7 ? true : false
+ end 
+
+ def clean?
+    self.hygiene > 7 ? true : false
+ end 
+
+ def get_paid(amount)
+    self.bank_account += amount
+    return "all about the benjamins"
 end 
-  
-def get_paid (salary)
-    @bank_account += salary
-    p "all about the benjamins"
-  end
 
-  def take_bath
-    self.hygiene =(@hygiene += 4)
-    p "♪ Rub-a-dub just relaxing in the tub ♫"
-   
-  end
-
-  def work_out 
-    @hygiene -= 3
-    @happiness +=2  
-    self.hygiene = (@hygiene)
-    self.happiness = (@happiness)
-    p "♪ another one bites the dust ♫"
-end 
-
-def call_friend(callee)
-    @happiness  += 3 
-    self.happiness = (@happiness)
-    callee.happiness += 3
-     p "Hi #{callee.name}! It's #{@name}. How are you?"
-    
+def take_bath 
+   self.hygiene += 4 unless self.hygiene >= 10 
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
 end
 
-def start_conversation (callee_name , topic)
+def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+  return  "♪ another one bites the dust ♫"
+end 
+
+def call_friend(person)
+  self.happiness += 3
+  person.happiness += 3
+  return "Hi #{person.name}! It's #{self.name}. How are you?"
+end 
+
+def start_conversation(person, topic)
     if topic == "politics"
-        callee_name.happiness -= 2 
-        @happiness -= 2
-        self.happiness = (@happiness)
-     return  'blah blah partisan blah lobbyist'
-      elsif topic == "weather"
-        callee_name.happiness += 1
-        @happiness += 1
-        self.happiness = (@happiness)
-        return  "blah blah sun blah rain"
-      else
-        return  'blah blah blah blah blah'
-
-        end
-   
+        self.happiness -= 2
+        person.happiness -= 2
+        return 'blah blah partisan blah lobbyist'
+    elsif topic == "weather"
+        self.happiness += 1
+        person.happiness += 1
+      return 'blah blah sun blah rain'
+    else
+      return "blah blah blah blah blah"
+    end
 
 
-    
-end
-
-
+end 
 
 end
